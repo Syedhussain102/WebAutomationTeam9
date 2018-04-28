694624f9cbe472dd7f3eafd393aebb31e0f4a782
Shubha Saha <Saha811@gmail.com>
2018-04-28 03:32:58 -0400

    errors

---
+++ CityBank/src/test/java/citybank/testDebitCards.java
@@ -0,0 +1,28 @@
+package citybank;
+
+import base.CommonAPI;
+import org.testng.annotations.Test;
+
+public class testDebitCards {
+
+    @Test
+    public void clickdebitcardTag() {
+        //clickButtonByXpath("#creditCards>a");
+    }
+    @Test
+    public void searchByCSS() {
+        //searchByCSS("#cA-cardsUseridMasked","UserID");
+    }
+    @Test
+    public void searchByXpath(){
+        //searchByXpath(".//*[@id='PASSWORD']","password");
+    }
+    @Test
+    public void clickButtonByCSS(){
+        //searchByCSS(".cA-cardsLoginSubmit.cA-DD-singon-buttom");
+    }
+    @Test
+    public void clickButtonByXpath(){
+        //clickButtonByXpath(".cA-DD-browseCards");
+    }
+}
---
+++ CityBank/src/test/java/citybank/testHomePage.java
@@ -0,0 +1,65 @@
+package citybank;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class testHomePage {
+
+    @Test
+    public void CitiBankTitle() {
+        String titleActual = "Banking with Citi | Citi.com";
+        //Assert.assertEquals(titleActual, driver.getTitle());
+        //System.out.println(driver.getTitle());
+    }
+    @Test
+    public void Citibankurl(){
+        String urlActual = "https://online.citi.com/US/login.do";
+        //Assert.assertEquals(urlActual,driver.getCurrentUrl());
+        //System.out.println(driver.getCurrentUrl());
+    }
+    @Test
+    public void Citibanklogo() {
+        String logoActual = "https://online.citi.com/US/login.do";
+        //System.out.println(driver.getPageSource());
+    }
+    @Test
+    public void clickcreditcardTag() {
+        //clickbuttonbyCSS("#creditCards>a");
+
+    }
+    @Test
+    public void CreditcardTitle(){
+        String ActualTitle = "Banking with Citi | Citi.com";
+        //Assert.assertEquals(ActualTitle,driver.getTitle());
+        //System.out.println(driver.getTitle());
+    }
+    @Test
+    public void clickBankingTab(){
+        //navigateback();
+        //clickbuttonbyCSS("#banking>a");
+
+    }
+    @Test
+    public void clicklandingTab() {
+        //navigateback();
+        //clickbuttonbyCSS("#lending");
+
+    }
+    @Test
+    public void clickinvestingTab() {
+        //navigateback();
+        //clickbuttonbyCSS("#investing");
+    }
+    @Test
+    public void clickcitigoldTab() {
+        //navigateback();
+        //clickbuttonbyCSS("#primary-nav>ul>li>a");
+    }
+    @Test
+    public void brokenlinksInHP(){
+       // brokenLinks();
+    }
+
+
+}
