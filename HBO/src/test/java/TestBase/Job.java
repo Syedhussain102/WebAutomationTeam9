60aac98a7433ef5df79ac49b91712d9c484a7974
syedhussain102 <syedhussain390@gmail.com>
2018-04-27 23:30:04 -0400

    mmmm

--- HBO/src/test/java/TestBase/Job.java
+++ HBO/src/test/java/TestBase/Job.java
@@ -1,5 +1,20 @@
 package TestBase;
 
-public class Job {
+import base.CommonAPI;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+public class Job extends CommonAPI {
+    @Test
+    public void lookForJob(){
+        clickByXpath("/html//main/div[11]/div/div//ul[@class='modules/FooterLinks--links modules/FooterLinks--topLinks']//a[@href='https://hbocareers.com/']");
+        switchTabs(0,1);
+        driver.findElement(By.xpath("/html/body/main/section[1]/div/div[2]/div/form/table/tbody/tr/td[1]/input")).sendKeys("SOFTWARE ENGINEER",Keys.ENTER);
+        driver.manage().window().fullscreen();
+        clickByXpath("//button[@class='ui-multiselect ui-widget ui-state-default ui-corner-all ui-state-active']");
+        clickByXpath("/html/body/div[6]/div/ul/li[1]/a/span[2]");
+
+    }
 
 }
