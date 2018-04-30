c0a21e01f92291d6370532a0b02d83a0f415d50a
Naim Khan Siam <37506695+NaimKhanSiam@users.noreply.github.com>
2018-04-30 17:57:22 -0400

    Syed (#5)
    * message
    
    * git
    
    * bracket

--- HBO/src/main/java/TestBase/mainClass.java
+++ HBO/src/main/java/TestBase/mainClass.java
@@ -13,18 +13,16 @@ public class mainClass extends CommonAPI {
 
    public void searchItems() {
        clickByXpath("/html/body/main/div[1]/div/div/header/div[1]/div/div[2]/div/div/div");
-        List<String> itemsList = new ArrayList<String>();
-        itemsList.add("West world");
-        itemsList.add("Kong");
-        itemsList.add("Fantastic Beast and Where TO Find Them");
-        for (String loopstring : itemsList) {
-            driver.findElement(By.xpath("/html//main[@class='wrapperMain']/div[1]/div/div//div[@class='components/Search--searchInputContainer']/input[@type='text']")).sendKeys(loopstring,Keys.ENTER);
-            clickByXpath("/html/body/main/div[1]/div/div/header/div[1]/div/div[2]/div/div/div");
+       List<String> itemsList = new ArrayList<String>();
+       itemsList.add("West world");
+       itemsList.add("Kong");
+       itemsList.add("Fantastic Beast and Where TO Find Them");
+       for (String loopstring : itemsList) {
+           driver.findElement(By.xpath("/html//main[@class='wrapperMain']/div[1]/div/div//div[@class='components/Search--searchInputContainer']/input[@type='text']")).sendKeys(loopstring, Keys.ENTER);
+           clickByXpath("/html/body/main/div[1]/div/div/header/div[1]/div/div[2]/div/div/div");
+
+       }
 
 
-        }
-//xx
-
-
-
+   }
 }
