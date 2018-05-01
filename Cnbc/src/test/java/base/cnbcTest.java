f53bcc820a27d6fd375a3535a8fc59c7c053331d
syedhussain102 <syedhussain390@gmail.com>
2018-04-30 20:24:07 -0400

    8:23 pm

---
+++ Cnbc/src/test/java/base/cnbcTest.java
@@ -0,0 +1,36 @@
+package base;
+
+import TestBase.mainClass;
+import org.testng.annotations.Test;
+
+public class cnbcTest extends mainClass {
+    @Test
+    public void homeTest(){homeUs();}
+    @Test
+    public void marketTest(){markets();}
+    @Test
+    public void investingTest(){ investing();}
+    @Test
+    public void (){ }
+    @Test
+    public void home(){ }
+    @Test
+    public void home(){ }
+    @Test
+    public void home(){ }
+    @Test
+    public void home(){ }
+    @Test
+    public void home(){ }
+    @Test
+    public void home(){ }
+    @Test
+    public void home(){ }
+    @Test
+    public void home(){ }
+    @Test
+    public void home(){ }
+    @Test
+    public void home(){ }
+
+}
