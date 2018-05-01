1ed28224233a050e413e56074b3cf8ed5ae0121e
“NaimKhanSiam” <naim_khan@live.co.uk>
2018-04-30 21:36:04 -0400

    monday

--- Ebay/src/main/java/main/ExcelSearch.java
+++ Ebay/src/main/java/main/ExcelSearch.java
@@ -1,52 +1,22 @@
 package main;
 
 import base.CommonAPI;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import reporting.TestLogger;
+import org.openqa.selenium.By;
 import utility.DataReader;
 
 import java.io.IOException;
 
 public class ExcelSearch extends CommonAPI{
-    //search from excel sheet
+    DataReader dr = new DataReader();
     public String[] getDataFromExcelFile() throws IOException {
-        DataReader dr = new DataReader();
-        String path = System.getProperty("user.dir") + "/data/Book1.xls";
-        String[] data = dr.fileReader(path);
+        String path = System.getProperty("user.dir")+"/data/file1.xls";
+        String [] data = dr.fileReader2(path,1);
         return data;
     }
 
-    /*public String[] getDataFromExcelFileForFeaturesChoice() throws IOException {
-        String path = System.getProperty("user.dir") + "/data/file1.xls";
-        String[] data = dr.fileReader2(path, 1);
+    public String[] getDataFromExcelFileForFeaturesChoice() throws IOException{
+        String path = System.getProperty("user.dir")+"data/file1.xls";
+        String [] data = dr.fileReader2(path,1);
         return data;
     }
-*/
-    @FindBy(xpath = "//input[@id='gh-ac']")
-    public static WebElement search;
-
-    public void clear() {
-        search.clear();
-    }
-    public void click(String value) {
-        search.sendKeys(value,Keys.ENTER);
-    }
-    public void searchItemsAndSubmitButton()throws IOException {
-        String [] valuee = getDataFromExcelFile();
-        for(int i=0; i<valuee.length; i++) {
-            click(valuee[i]);
-           clear();
-        }
-    }
-
-    /*public void searchItemsAndSubmitButton()throws IOException {
-        clickByXpath( "//input[@id='gh-ac']");
-        String [] value = getDataFromExcelFile();
-        for(int i=0; i<value.length; i++) {
-            typeByXpath("//input[@id='gh-ac']",value[i]);
-            clickNClearbyXpath( "//input[@id='gh-ac']");
-        }
-    }*/
 }
--- Ebay/src/test/java/ebay/ExcelSearchTest.java
+++ Ebay/src/test/java/ebay/ExcelSearchTest.java
@@ -1,11 +1,14 @@
 package ebay;
 
 import base.CommonAPI;
+import main.ExcelSearch;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-public class ExcelSearchTest extends CommonAPI {
+public class ExcelSearchTest extends ExcelSearch {
     //@Test
     public void testExcel() throws IOException {
+        searchItemsAndSubmitButton();
     }
 }
