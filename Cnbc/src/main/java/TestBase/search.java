e7d2a2b0f2ee83f9946da6f01b735a9f29fc9bcc
syedhussain102 <syedhussain390@gmail.com>
2018-05-02 17:49:30 -0400

    wed 5:49pm

--- Cnbc/src/main/java/TestBase/search.java
+++ Cnbc/src/main/java/TestBase/search.java
@@ -1,16 +1,27 @@
 package TestBase;
 
 import base.CommonAPI;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class search extends CommonAPI {
+    //search single iteam
+    public void itemSearch(){
+        typeByXpathNEnter("//*[@placeholder='Search Quotes, News & Video']","gaming");
+        //driver.findElement(By.xpath("//*[@placeholder='Search Quotes, News & Video']")).sendKeys("VERIZON",Keys.ENTER);
+    }
     //SearchMultipleItems
     @FindBy(xpath = "//*[@placeholder='Search Quotes, News & Video']")
     public WebElement searchElements;
@@ -24,7 +35,24 @@ public class search extends CommonAPI {
             //driver.findElement(By.xpath("//*[@class='fa fa-search fa-2']")).click();
             searchElements.clear();
         }
-
+    }
+    public ArrayList<String> dataFromExcel(int colNo) throws IOException {
+        FileInputStream fis = new FileInputStream("/Users/sreejon/IdeaProjects/WebAutomationTeam9/Cnbc/cnbcData/CnbcBook-1.xls");
+        HSSFWorkbook wb = new HSSFWorkbook(fis);
+        HSSFSheet s = wb.getSheet("CnbcBook");
+        Iterator<Row> rowIT =s.iterator();
+        ArrayList<String> list = new ArrayList<>();
+        while(rowIT.hasNext()){
+            list.add(rowIT.next().getCell(colNo).getStringCellValue());
+        }
+        return list;
+    }
+    public void excelTest() throws IOException {
+        ArrayList<String> searchAbleItems = dataFromExcel(0);
+        for (int i = 0; i < searchAbleItems.size(); i++) {
+            typeByXpathNEnter("//*[@placeholder='Search Quotes, News & Video']", searchAbleItems.get(i));
+            clearInputByXpath("//*[@placeholder='Search Quotes, News & Video']");
+        }
     }
     public void work(){
         Actions action = new Actions(driver);
--- Cnbc/src/test/java/base/searchTest.java
+++ Cnbc/src/test/java/base/searchTest.java
@@ -4,10 +4,19 @@ import TestBase.search;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class searchTest extends search {
-    @Test
-    public void test3() {
+    //@Test
+    public void searchMultipleIteams() {
         search sc = PageFactory.initElements(driver, search.class);
         sc.searchItems();
+    }
+    //@Test
+    public void searchOneItem(){ searchItems();}
+    @Test
+    public void exTest() throws IOException {
+        excelTest();
+
     }
 }
